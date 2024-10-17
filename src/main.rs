380743188caed7428ffee8e0d802a516b9b7cb15
Edwin <Edwin.v.velthoven@gmail.com>
2024-10-17 16:51:35 +0200

    Finished solution for project euler

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,36 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
-// What is the largest prime factor of the number 600851475143?
+// What is the largest prime factor of the number 600.851.475.143?
 
 fn main() {
-    let value: u64 = 600851475143;
+    let mut value: u64 = 600851475143;
 
     let mut prime_number: u64 = 2;
-    let mut highest_prime_number: u32 = 2;
+    // let mut highest_prime_number: u32 = 2;
+    let mut factor_list: Vec<u64> = Vec::new();
+  
+    //  pow = prime_number * prime_number * ...
+    while value > 1 && prime_number.pow(2) <= value {
+        // Check if divided evenly
+        if value % prime_number == 0 {
+            // if evenly push to list
+            factor_list.push(prime_number);
+            // divide value and set it
+            value /= prime_number;
 
-    let mut used_prime_numbers: Vec<u64> = Vec::new();
-    
-
-    // while loop (if value is bigger than 1)
-    while value > 1 {
-
-        // check if number is a prime number (only dividable with it's self or 1)
-        if prime_number % 2 != 1 && prime_number != 2 {
-            println!("Skipped {:?} number is not odd or 2", prime_number);
-            prime_number += 1;
-            continue;
+            println!("{:?}", factor_list);
         }
-
-        let result = (value as f64) % (prime_number as f64);
-        // check if result has decimals (skip if it has)
-        if result.fract() != 0.0 {
-            println!("Result has decimals, Skipped");
-            prime_number += 1;
-            continue;
+        // This should improve performance??? not sure...
+        else if prime_number > 2  {
+            prime_number += 2;
         }
-
-        // divide value with biggest possible prime number
-        println!("We are finding prime numbers with this one");
-        println!("{:?}", result);
-            // check if new value is a full number
-        used_prime_numbers.push(prime_number);
-        // println!("{:?}", used_prime_numbers);
-        prime_number += 1;
-            // update value if true, check again if false
-
+        else {
+            prime_number += 1;
+        }
     }
-    // Print result
+    if value > 1 {
+        factor_list.push(value);
+    }
+
+    println!("Ended with {:?}", factor_list);
 
 }
